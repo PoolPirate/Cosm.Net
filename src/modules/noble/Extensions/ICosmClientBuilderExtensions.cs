ce50e1769925fe9b1c58ea777a4c7bee6e545748
Playwo <eliaswolf1609@gmail.com>
2024-02-01 17:43:44 +0100

    feat: Noble support

---
+++ src/modules/noble/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Extensions;
+using Cosm.Net.Noble.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Noble.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallNoble(this CosmClientBuilder builder, string bech32Prefix = "noble")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix)
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
---
+++ src/modules/noble/Modules/NobleModules.cs
@@ -0,0 +1,6 @@
+﻿using Cosm.Net.Modules;
+
+namespace Cosm.Net.Noble.Modules;
+internal partial class TariffModule : IModule<TariffModule, global::Noble.Tariff.Query.QueryClient> { }
+internal partial class GlobalFeeModule : IModule<GlobalFeeModule, global::Noble.Globalfee.Query.QueryClient> { }
+internal partial class TokenFactoryModule : IModule<TokenFactoryModule, global::Noble.Tokenfactory.Query.QueryClient> { }
