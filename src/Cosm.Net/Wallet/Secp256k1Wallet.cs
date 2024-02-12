c00e07685c59991b0b31b3a99a350d9bab8214b0
Playwo <eliaswolf1609@gmail.com>
2024-02-12 11:05:08 +0100

    feat: Allow configuring coinType & accountIndex in wallet

--- src/Cosm.Net/Wallet/Secp256k1Wallet.cs
+++ src/Cosm.Net/Wallet/Secp256k1Wallet.cs
@@ -11,12 +11,13 @@ namespace Cosm.Net.Wallet;
 public sealed class Secp256k1Wallet : IOfflineSigner
 {
     private static readonly Secp256k1 Secp256k1 = new Secp256k1();
+    private static readonly WalletOptions DefaultWalletOptions = new WalletOptions();
 
     public byte[] PrivateKey { get; }
     public byte[] PublicKey { get; }
 
-    private readonly byte[] UncompressedPublicKey;
-    private readonly byte[] AddressBytes;
+    private readonly byte[] _uncompressedPublicKey;
+    private readonly byte[] _addressBytes;
 
     public Secp256k1Wallet(Span<byte> privateKey)
     {
@@ -32,9 +33,9 @@ public sealed class Secp256k1Wallet : IOfflineSigner
         PrivateKey = privateKey.ToArray(); //Prevent user from changing private key after ctor
         PublicKey = new byte[Secp256k1.SERIALIZED_COMPRESSED_PUBKEY_LENGTH];
 
-        UncompressedPublicKey = new byte[Secp256k1.PUBKEY_LENGTH];
-        if(!Secp256k1.PublicKeyCreate(UncompressedPublicKey, privateKey) ||
-           !Secp256k1.PublicKeySerialize(PublicKey, UncompressedPublicKey, Flags.SECP256K1_EC_COMPRESSED))
+        _uncompressedPublicKey = new byte[Secp256k1.PUBKEY_LENGTH];
+        if(!Secp256k1.PublicKeyCreate(_uncompressedPublicKey, privateKey) ||
+           !Secp256k1.PublicKeySerialize(PublicKey, _uncompressedPublicKey, Flags.SECP256K1_EC_COMPRESSED))
         {
             throw new ArgumentException("Failed to derive public key from private key", nameof(privateKey));
         }
@@ -43,20 +44,21 @@ public sealed class Secp256k1Wallet : IOfflineSigner
 
         var ripeMd160 = new RipeMD160Digest();
         ripeMd160.BlockUpdate(pubkeyHash, 0, pubkeyHash.Length);
-        AddressBytes = new byte[ripeMd160.GetDigestSize()];
-        _ = ripeMd160.DoFinal(AddressBytes, 0);
+        _addressBytes = new byte[ripeMd160.GetDigestSize()];
+        _ = ripeMd160.DoFinal(_addressBytes, 0);
     }
 
-    public Secp256k1Wallet(string mnemonic, string passphrase = "", BIP39Wordlist wordlist = BIP39Wordlist.English)
-        : this(DerivePrivateKeyFromMnemonic(mnemonic, passphrase, wordlist))
+    public Secp256k1Wallet(string mnemonic, string passphrase = "", WalletOptions? options = null)
+        : this(DerivePrivateKeyFromMnemonic(mnemonic, passphrase, 
+            options?.CoinType ?? DefaultWalletOptions.CoinType, options?.AccountIndex ?? DefaultWalletOptions.AccountIndex))
     {
     }
 
-    private static byte[] DerivePrivateKeyFromMnemonic(string mnemonic, string passphrase, BIP39Wordlist wordlist)
+    private static byte[] DerivePrivateKeyFromMnemonic(string mnemonic, string passphrase, int coinType, int accountIndex)
     {
         //ToDo: Validate mnemonic
         byte[] seed = BIP39.MnemonicToSeed(mnemonic, passphrase);
-        string path = $"m/{44 + 2147483648u}'/{118 + 2147483648u}'/{0 + 2147483648u}'/0'/0'";
+        string path = $"m/{44 + 2147483648u}'/{coinType + 2147483648u}'/{0 + 2147483648u}'/0'/{accountIndex}'";
 
         var (key, _) = BIP32.DerivePath(seed, path, BIP32Curves.Secp256k1);
         return key;
@@ -92,6 +94,6 @@ public sealed class Secp256k1Wallet : IOfflineSigner
     }
 
     public string GetAddress(string prefix)
-        => Bech32.EncodeAddress(prefix, AddressBytes)
+        => Bech32.EncodeAddress(prefix, _addressBytes)
             ?? throw new InvalidOperationException("Creating address failed");
 }
---
+++ src/Cosm.Net/Wallet/WalletOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Cosm.Net.Wallet;
+public class WalletOptions
+{
+    public int CoinType { get; init; } = 118;
+    public int AccountIndex { get; init; } = 0;
+}
