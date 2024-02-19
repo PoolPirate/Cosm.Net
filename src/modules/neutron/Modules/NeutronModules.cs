53f96d10d4ab1dfadb8d785e37e80d6a2159c831
Playwo <eliaswolf1609@gmail.com>
2024-02-19 15:33:00 +0100

    feat: Neutron chain package

---
+++ src/modules/neutron/Modules/NeutronModules.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosm.Net.Modules;
+internal partial class ContractManagerModule : IModule<ContractManagerModule, global::Neutron.Contractmanager.Query.QueryClient> { }
+internal partial class CronModule : IModule<CronModule, global::Neutron.Cron.Query.QueryClient> { }
+internal partial class DexModule : IModule<DexModule, global::Neutron.Dex.Query.QueryClient> { }
+internal partial class FeeBurnerModule : IModule<FeeBurnerModule, global::Neutron.Feeburner.Query.QueryClient> { }
+internal partial class FeeRefunderModule : IModule<FeeRefunderModule, global::Neutron.Feerefunder.Query.QueryClient> { }
+internal partial class InterchainQueriesModule : IModule<InterchainQueriesModule, global::Neutron.Interchainqueries.Query.QueryClient> { }
+internal partial class InterchainTxsModule : IModule<InterchainTxsModule, global::Neutron.Interchaintxs.V1.Query.QueryClient> { }
+internal partial class TransferModule : IModule<TransferModule, global::Neutron.Transfer.Query.QueryClient> { }
