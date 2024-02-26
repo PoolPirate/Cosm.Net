225139a5ab8367691342d37d12a32d40fa4c9992
Playwo <eliaswolf1609@gmail.com>
2024-02-26 22:21:04 +0100

    fix: Typo in IBC Modules names

--- src/modules/sei/Modules/IbcGoModules.cs
+++ src/modules/sei/Modules/IbcGoModules.cs
@@ -1,11 +1,10 @@
 ﻿namespace Cosm.Net.Modules;
+internal partial class IbcFeeModule : IModule<IbcFeeModule, global::Ibc.Applications.Fee.V1.Query.QueryClient> { }
 internal partial class IbcInterchainAccountsControllerModule
-    : IModule<IbcInterchainAccountsControllerModule, global::Ibc.Applications.InterchainAccounts.Controller.V1.Query.QueryClient>
-{ }
+    : IModule<IbcInterchainAccountsControllerModule, global::Ibc.Applications.InterchainAccounts.Controller.V1.Query.QueryClient> { }
 internal partial class IbcInterchainAccountsHostModule
-    : IModule<IbcInterchainAccountsHostModule, global::Ibc.Applications.InterchainAccounts.Host.V1.Query.QueryClient>
-{ }
+    : IModule<IbcInterchainAccountsHostModule, global::Ibc.Applications.InterchainAccounts.Host.V1.Query.QueryClient> { }
 internal partial class IbcTransferModule : IModule<IbcTransferModule, global::Ibc.Applications.Transfer.V1.Query.QueryClient> { }
-internal partial class IbChannelModule : IModule<IbChannelModule, global::Ibc.Core.Channel.V1.Query.QueryClient> { }
+internal partial class IbcChannelModule : IModule<IbcChannelModule, global::Ibc.Core.Channel.V1.Query.QueryClient> { }
 internal partial class IbcClientModule : IModule<IbcClientModule, global::Ibc.Core.Client.V1.Query.QueryClient> { }
-internal partial class IbConnectionModule : IModule<IbConnectionModule, global::Ibc.Core.Connection.V1.Query.QueryClient> { }
+internal partial class IbcConnectionModule : IModule<IbcConnectionModule, global::Ibc.Core.Connection.V1.Query.QueryClient> { }
