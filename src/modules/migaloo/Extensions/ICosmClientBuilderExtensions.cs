5246302154ed72647643816ee940aefa77461610
Playwo <eliaswolf1609@gmail.com>
2024-03-23 13:11:31 +0100

    feat: Add migaloo chain package

---
+++ src/modules/migaloo/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallMigaloo(this CosmClientBuilder builder, string bech32Prefix = "migaloo")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix)
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IWasmAdapater, WasmModule>()
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
