32837aa1dc3bef158f190486863c4882a4a889ab
Playwo <eliaswolf1609@gmail.com>
2024-04-19 13:52:59 +0200

    feat: Archway chain package

---
+++ src/modules/archway/Modules/ArchwayModules.cs
@@ -0,0 +1,4 @@
+﻿namespace Cosm.Net.Modules;
+
+internal partial class RewardsModule : IModule<RewardsModule, global::Archway.Rewards.V1.Query.QueryClient> { }
+internal partial class TrackingModule : IModule<TrackingModule, global::Archway.Tracking.V1.Query.QueryClient> { }
