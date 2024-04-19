4553f31a1b62ea0827ecc204c8dc6af2daf3f183
Playwo <eliaswolf1609@gmail.com>
2024-04-19 13:22:01 +0200

    feat: Update Osmosis to v24.0.1

--- src/modules/osmosis/Modules/OsmosisModules.cs
+++ src/modules/osmosis/Modules/OsmosisModules.cs
@@ -1,15 +1,18 @@
 ﻿namespace Cosm.Net.Modules;
 
+internal partial class BridgeModule : IModule<BridgeModule, global::Osmosis.Bridge.V1Beta1.Query.QueryClient> { }
 internal partial class ConcentratedLiquidityModule : IModule<ConcentratedLiquidityModule, global::Osmosis.Concentratedliquidity.V1Beta1.Query.QueryClient> { }
 internal partial class DowntimeDetectorModule : IModule<DowntimeDetectorModule, global::Osmosis.Downtimedetector.V1Beta1.Query.QueryClient> { }
 internal partial class EpochsModule : IModule<EpochsModule, global::Osmosis.Epochs.V1Beta1.Query.QueryClient> { }
 internal partial class GammModule : IModule<GammModule, global::Osmosis.Gamm.V1Beta1.Query.QueryClient> { }
+internal partial class GammV2Module : IModule<GammV2Module, global::Osmosis.Gamm.V2.Query.QueryClient> { }
 internal partial class IbcRateLimitModule : IModule<IbcRateLimitModule, global::Osmosis.Ibcratelimit.V1Beta1.Query.QueryClient> { }
 internal partial class IncentivesModule : IModule<IncentivesModule, global::Osmosis.Incentives.Query.QueryClient> { }
 internal partial class LockupModule : IModule<LockupModule, global::Osmosis.Lockup.Query.QueryClient> { }
 internal partial class MintModule : IModule<MintModule, global::Osmosis.Mint.V1Beta1.Query.QueryClient> { }
 internal partial class PoolIncentivesModule : IModule<PoolIncentivesModule, global::Osmosis.Poolincentives.V1Beta1.Query.QueryClient> { }
 internal partial class PoolManagerModule : IModule<PoolManagerModule, global::Osmosis.Poolmanager.V1Beta1.Query.QueryClient> { }
+internal partial class PoolManagerV2Module : IModule<PoolManagerV2Module, global::Osmosis.Poolmanager.V2.Query.QueryClient> { }
 internal partial class ProtoRevModule : IModule<ProtoRevModule, global::Osmosis.Protorev.V1Beta1.Query.QueryClient> { }
 internal partial class SuperfluidModule : IModule<SuperfluidModule, global::Osmosis.Superfluid.Query.QueryClient> { }
 internal partial class TokenFactoryModule : IModule<TokenFactoryModule, global::Osmosis.Tokenfactory.V1Beta1.Query.QueryClient> { }
