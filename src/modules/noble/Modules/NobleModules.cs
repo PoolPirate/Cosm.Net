faab5e91532e14e9e28e9bda6243a12864b585f8
Playwo <eliaswolf1609@gmail.com>
2024-04-19 13:15:26 +0200

    feat: Update noble to 4.1.1

--- src/modules/noble/Modules/NobleModules.cs
+++ src/modules/noble/Modules/NobleModules.cs
@@ -1,4 +1,5 @@
 ﻿namespace Cosm.Net.Modules;
 internal partial class TariffModule : IModule<TariffModule, global::Noble.Tariff.Query.QueryClient> { }
-//internal partial class GlobalFeeModule : IModule<GlobalFeeModule, global::Noble.Globalfee.Query.QueryClient> { }
+internal partial class GlobalFeeModule : IModule<GlobalFeeModule, global::Noble.Globalfee.Query.QueryClient> { }
 internal partial class TokenFactoryModule : IModule<TokenFactoryModule, global::Noble.Tokenfactory.Query.QueryClient> { }
+internal partial class ForwardingModule : IModule<ForwardingModule, global::Noble.Forwarding.V1.Query.QueryClient> { }
