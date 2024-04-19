cd63e3af624eab24398fb5ec1cfaaaec1602cf93
Playwo <eliaswolf1609@gmail.com>
2024-04-19 13:09:20 +0200

    feat: Kujira chain package

---
+++ src/modules/kujira/Modules/KujiraModules.cs
@@ -0,0 +1,4 @@
+﻿namespace Cosm.Net.Modules;
+internal partial class SchedulerModule : IModule<SchedulerModule, global::Kujira.Scheduler.Query.QueryClient> { }
+internal partial class OracleModule : IModule<OracleModule, global::Kujira.Oracle.Query.QueryClient> { }
+internal partial class DenomModule : IModule<DenomModule, global::Kujira.Denom.Query.QueryClient> { }
