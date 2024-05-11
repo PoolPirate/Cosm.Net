0d6d5e18cb2317720fb7aed9b82348be7384b28d
Playwo <eliaswolf1609@gmail.com>
2024-05-11 22:10:54 +0200

    feat: Omniflix chain package

---
+++ src/modules/omniflix/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using Cosm.Net.Services;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallOmniflix(this CosmClientBuilder builder, string bech32Prefix = "omniflix")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix)
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IWasmAdapater, WasmModule>()
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
