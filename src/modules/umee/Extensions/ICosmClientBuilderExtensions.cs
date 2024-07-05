a47e364f75e7b0bb70974858080a96aed68c4a5b
Playwo <eliaswolf1609@gmail.com>
2024-07-05 11:59:39 +0200

    feat: Add Umee chain module

---
+++ src/modules/umee/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallUmee(this CosmClientBuilder builder, string bech32Prefix = "umee")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IWasmAdapater, WasmModule>()
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
---
+++ src/modules/umee/Modules/UmeeModules.cs
@@ -0,0 +1,7 @@
+﻿namespace Cosm.Net.Modules;
+internal partial class IncentiveModule : IModule<IncentiveModule, Umee.Incentive.V1.Query.QueryClient> { }
+internal partial class LeverageModule : IModule<LeverageModule, Umee.Leverage.V1.Query.QueryClient> { }
+internal partial class MetatokenModule : IModule<MetatokenModule, Umee.Metoken.V1.Query.QueryClient> { }
+internal partial class OracleModule : IModule<OracleModule, Umee.Oracle.V1.Query.QueryClient> { }
+internal partial class UGovModule : IModule<UGovModule, Umee.Ugov.V1.Query.QueryClient> { }
+internal partial class UIbcModule : IModule<UIbcModule, Umee.Uibc.V1.Query.QueryClient> { }
