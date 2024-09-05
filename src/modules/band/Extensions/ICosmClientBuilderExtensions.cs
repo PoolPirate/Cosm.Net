446f460bfcb725fa5d5bab6fe7188921bdb94e42
Playwo <eliaswolf1609@gmail.com>
2024-09-05 17:43:38 +0200

    feat: Band chain package

---
+++ src/modules/band/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallBand(this CosmClientBuilder builder, string bech32Prefix = "band")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
