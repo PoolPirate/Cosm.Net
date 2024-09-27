fb7a4788f0882c69d00508f8f858de9bbae1df3e
Playwo <eliaswolf2001@t-online.de>
2024-09-27 13:33:18 +0200

    feat: Celestia chain package

---
+++ src/modules/celestia/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallCelestia(this CosmClientBuilder builder, string bech32Prefix = "celestia")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
