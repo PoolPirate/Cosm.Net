184a244e69914860f5677bc648337b0ae0a573da
Playwo <eliaswolf1609@gmail.com>
2024-10-12 19:14:04 +0200

    feat: Add Skip modules to neutron

---
+++ src/modules/neutron/Modules/SkipModules.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosm.Net.Modules;
+internal partial class AuctionModule : IModule<AuctionModule, Sdk.Auction.V1.Query.QueryClient> { }
+internal partial class MempoolModule : IModule<MempoolModule, Sdk.Mempool.V1.Service.ServiceClient> { }
+internal partial class FeeMarketModule : IModule<FeeMarketModule, Feemarket.Feemarket.V1.Query.QueryClient> { }
+internal partial class SkipConnectAlertsModule : IModule<SkipConnectAlertsModule, Slinky.Alerts.V1.Query.QueryClient> { }
+internal partial class SkipConnectIncentivesModule : IModule<SkipConnectIncentivesModule, Slinky.Incentives.V1.Query.QueryClient> { }
+internal partial class SkipConnectMarketmapModule : IModule<SkipConnectMarketmapModule, Slinky.Marketmap.V1.Query.QueryClient> { }
+internal partial class SkipConnectOracleModule : IModule<SkipConnectOracleModule, Slinky.Oracle.V1.Query.QueryClient> { }
+internal partial class SkipConnectSlaModule : IModule<SkipConnectSlaModule, Slinky.Sla.V1.Query.QueryClient> { }
