6b9032f1dfb3319256a71a324d7f860304abe85c
Playwo <eliaswolf1609@gmail.com>
2024-10-21 13:20:14 +0200

    feat: Remove dex module from sei

--- src/modules/sei/Modules/SeiModules.cs
+++ src/modules/sei/Modules/SeiModules.cs
@@ -1,5 +1,4 @@
 ﻿namespace Cosm.Net.Modules;
-internal partial class DexModule : IModule<DexModule, Seiprotocol.Seichain.Dex.Query.QueryClient> { }
 internal partial class EpochModule : IModule<EpochModule, Seiprotocol.Seichain.Epoch.Query.QueryClient> { }
 internal partial class MintModule : IModule<MintModule, Seiprotocol.Seichain.Mint.Query.QueryClient> { }
 internal partial class OracleModule : IModule<OracleModule, Seiprotocol.Seichain.Oracle.Query.QueryClient> { }
