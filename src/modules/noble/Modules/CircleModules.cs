c16958425acf99ece247063c7530e0c54143091c
PoolPirate <eliaswolf1609@gmail.com>
2025-01-17 15:57:13 +0100

    feat: Add circle modules to Noble package

---
+++ src/modules/noble/Modules/CircleModules.cs
@@ -0,0 +1,4 @@
+﻿namespace Cosm.Net.Modules;
+
+internal partial class CCTPModule : IModule<CCTPModule, Circle.Cctp.V1.Query.QueryClient> { }
+internal partial class FiatTokenFactoryModule : IModule<FiatTokenFactoryModule, Circle.Fiattokenfactory.V1.Query.QueryClient> { }
