8a23328da9602b40e9c7d2ddfcc0d12dc585fbf7
PoolPirate <eliaswolf1609@gmail.com>
2025-03-31 21:34:16 +0200

    feat: Add Saga package

---
+++ src/modules/saga/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Cosm.Net.Adapters;
+using Cosm.Net.Client;
+using Cosm.Net.Modules;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallSaga(this CosmClientBuilder builder, string bech32Prefix = "saga")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().RegisterModule<IAuthModuleAdapter, AuthModule>()
+            .AsInternal().RegisterModule<ITendermintModuleAdapter, TendermintModule>()
+            .AsInternal().RegisterModule<ITxModuleAdapter, TxModule>();
+}
