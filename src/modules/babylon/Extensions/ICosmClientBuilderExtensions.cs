7f47c4176d2ae441ac9f3eef3171d4491cfba1f4
PoolPirate <eliaswolf1609@gmail.com>
2025-04-10 15:02:44 +0200

    feat: Babylon chain package

---
+++ src/modules/babylon/Extensions/ICosmClientBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Cosm.Net.Client;
+using System.Reflection;
+
+namespace Cosm.Net.Extensions;
+public static class ICosmClientBuilderExtensions
+{
+    public static CosmClientBuilder InstallBabylon(this CosmClientBuilder builder, string bech32Prefix = "bbn")
+        => builder
+            .AsInternal().UseCosmosTxStructure()
+            .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly());
+}
