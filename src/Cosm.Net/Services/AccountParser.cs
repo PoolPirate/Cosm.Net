7603e8afa5086db355338a28529afe80e7f7e585
PoolPirate <eliaswolf1609@gmail.com>
2025-05-23 12:18:08 +0200

    feat: support more account types

--- src/Cosm.Net/Services/AccountParser.cs
+++ src/Cosm.Net/Services/AccountParser.cs
@@ -35,8 +35,31 @@ public sealed class AccountParser
             return handler(account);
         }
 
-        return account.TryUnpack<Cosmos.Auth.V1Beta1.BaseAccount>(out var baseAccount)
-            ? new AccountData(baseAccount.AccountNumber, baseAccount.Sequence)
-            : throw new InvalidOperationException($"Cannot parse account type: {account.TypeUrl}");
+        if(account.TryUnpack<Cosmos.Auth.V1Beta1.BaseAccount>(out var baseAccount))
+        {
+            return new AccountData(baseAccount.AccountNumber, baseAccount.Sequence);
+        }
+        else if(account.TryUnpack<Cosmos.Vesting.V1Beta1.ContinuousVestingAccount>(out var continousVestingAccount))
+        {
+            return new AccountData(continousVestingAccount.BaseVestingAccount.BaseAccount.AccountNumber, continousVestingAccount.BaseVestingAccount.BaseAccount.Sequence);
+        }
+        else if(account.TryUnpack<Cosmos.Vesting.V1Beta1.DelayedVestingAccount>(out var delayedVestingAccount))
+        {
+            return new AccountData(delayedVestingAccount.BaseVestingAccount.BaseAccount.AccountNumber, delayedVestingAccount.BaseVestingAccount.BaseAccount.Sequence);
+        }
+        else if(account.TryUnpack<Cosmos.Vesting.V1Beta1.BaseVestingAccount>(out var baseVestingAccount))
+        {
+            return new AccountData(baseVestingAccount.BaseAccount.AccountNumber, baseVestingAccount.BaseAccount.Sequence);
+        }
+        else if(account.TryUnpack<Cosmos.Vesting.V1Beta1.PermanentLockedAccount>(out var permanentLockedAccount))
+        {
+            return new AccountData(permanentLockedAccount.BaseVestingAccount.BaseAccount.AccountNumber, permanentLockedAccount.BaseVestingAccount.BaseAccount.Sequence);
+        }
+        else if(account.TryUnpack<Cosmos.Vesting.V1Beta1.PeriodicVestingAccount>(out var periodicVestingAccount))
+        {
+            return new AccountData(periodicVestingAccount.BaseVestingAccount.BaseAccount.AccountNumber, periodicVestingAccount.BaseVestingAccount.BaseAccount.Sequence);
+        }
+        //
+        throw new InvalidOperationException($"Cannot parse account type: {account.TypeUrl}");
     }
 }
