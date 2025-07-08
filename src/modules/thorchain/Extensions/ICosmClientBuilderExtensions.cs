b076739a7da05d7f367dfeb4c88fc2823ecf6aa1
PoolPirate <eliaswolf1609@gmail.com>
2025-07-08 14:11:34 +0200

    feat: Add constant gas fee provider to Thorchain

--- src/modules/thorchain/Extensions/ICosmClientBuilderExtensions.cs
+++ src/modules/thorchain/Extensions/ICosmClientBuilderExtensions.cs
@@ -1,12 +1,17 @@
 ﻿using Cosm.Net.Client;
+using Cosm.Net.Models;
+using Cosm.Net.Services;
 using System.Reflection;
 
 namespace Cosm.Net.Extensions;
 public static class ICosmClientBuilderExtensions
 {
-    public static CosmClientBuilder InstallThorchain(this CosmClientBuilder builder, string bech32Prefix = "thor")
+    public static CosmClientBuilder InstallThorchain(this CosmClientBuilder builder, string bech32Prefix = "thor", Coin? gasFee = null)
         => builder
             .AsInternal().UseCosmosTxStructure()
             .AsInternal().WithChainInfo(bech32Prefix, TimeSpan.FromSeconds(40))
-            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly());
+            .AsInternal().RegisterModulesFromAssembly(Assembly.GetExecutingAssembly())
+            .AsInternal().WithGasFeeProvider<ConstantGasFeeProvider, ConstantGasFeeProvider.Configuration>(
+                new ConstantGasFeeProvider.Configuration(gasFee ?? new Coin("rune", 2000000))
+            );
 }
---
+++ src/modules/thorchain/Services/ConstantGasFeeProvider.cs
@@ -0,0 +1,27 @@
+﻿using Cosm.Net.Configuration;
+using Cosm.Net.Models;
+
+namespace Cosm.Net.Services;
+internal class ConstantGasFeeProvider : IGasFeeProvider<ConstantGasFeeProvider.Configuration>
+{
+    public record Configuration(Coin GasFee);
+
+    public Coin GasFee { get; }
+
+    public string GasFeeDenom => GasFee.Denom;
+
+    private readonly IGasBufferConfiguration _gasBufferConfiguration;
+
+    public ConstantGasFeeProvider(Configuration configuration, IGasBufferConfiguration gasBufferConfiguration)
+    {
+        GasFee = configuration.GasFee;
+        _gasBufferConfiguration = gasBufferConfiguration;
+    }
+
+    public ValueTask<Coin> GetFeeForGasAsync(ulong gasWanted, CancellationToken cancellationToken = default)
+        => ValueTask.FromResult(GasFee);
+
+    public ulong ApplyGasBuffers(ulong gasWanted, double? gasMultiplier = null, ulong? gasOffset = null)
+        => (ulong) (gasWanted * (gasMultiplier ?? _gasBufferConfiguration.GasMultiplier))
+            + (gasOffset ?? _gasBufferConfiguration.GasOffset);
+}
