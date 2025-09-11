c7e899655e56c0d29bee2e78bb3466fedd5950e9
PoolPirate <eliaswolf1609@gmail.com>
2025-09-11 14:39:03 +0200

    feat: more robust tx module publisher

--- src/Cosm.Net/Services/TxModulePublisher.cs
+++ src/Cosm.Net/Services/TxModulePublisher.cs
@@ -2,6 +2,7 @@
 using Cosm.Net.Models;
 using Cosm.Net.Models.Tx;
 using Cosm.Net.Tx;
+using Grpc.Core;
 
 namespace Cosm.Net.Services;
 public class TxModulePublisher(IInternalTxAdapter txModule, ITxEncoder txEncoder)
@@ -11,10 +12,20 @@ public class TxModulePublisher(IInternalTxAdapter txModule, ITxEncoder txEncoder
     private readonly ITxEncoder _txEncoder = txEncoder;
 
     public async Task<TxSubmission> PublishTxAsync(ISignedCosmTx tx, DateTime? deadline, CancellationToken cancellationToken)
-        => await _txModule.BroadcastTxAsync(
-            _txEncoder.EncodeTx(tx),
-            BroadcastMode.Sync,
-            deadline: deadline,
-            cancellationToken: cancellationToken
-        );
+    {
+        try
+        {
+            return await _txModule.BroadcastTxAsync(
+                _txEncoder.EncodeTx(tx),
+                BroadcastMode.Sync,
+                deadline: deadline,
+                cancellationToken: cancellationToken
+            );
+        }
+        catch(RpcException ex) when(ex.StatusCode == StatusCode.Unknown && ex.Message.Contains("sequence mismatch"))
+        {
+            int txHashStartIdx = ex.Message.IndexOf("tx ") + 3;
+            return new TxSubmission(32, ex.Message.Substring(txHashStartIdx, 64), ex.Message);
+        }
+    }
 }
